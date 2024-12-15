5c8bc230e9a4e96447d3c9f8437e2bda3e62733b
VanteBar <valeryanka.04@mail.ru>
2024-12-15 19:43:49 +0300

    Страница регистрации

--- src/pages/PageReg.tsx
+++ src/pages/PageReg.tsx
@@ -5,23 +5,39 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export function PageReg()  {
     return (
-        <div className="RegForm">
-            <div><Label htmlFor="terms">Почта</Label></div>
-            <Input type="email" placeholder="Введите почту" />
-            <div><Label htmlFor="terms">Пароль</Label></div>
-            <Input type="email" placeholder="Введите пароль" />
-            <div><Label htmlFor="terms">Повторите пароль</Label></div>
-            <Input type="email" placeholder="Повторите пароль" />
-            <div><Label htmlFor="terms">Выберите роль</Label></div>
+        <div className="bg-[#f8fafc] flex justify-center items-center w-screen h-dvh">
+            <div className="bg-white flex-col w-416px h-486px rounded-[12px] p-3">
 
-            <Tabs defaultValue="account" className="w-[400px]">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="Reader">Читатель</TabsTrigger>
-                    <TabsTrigger value="Author">Автор</TabsTrigger>
-                </TabsList>
-            </Tabs>
+                <h3 className="decoration-[#0f172a] font-semibold">Создать аккаунт</h3>
+                <div className="pt-3">
+                    <div><Label htmlFor="terms">Почта</Label></div>
+                    <Input type="email" placeholder="Введите почту" />
+                </div>
+                <div className="pt-3">
+                    <div><Label htmlFor="terms">Пароль</Label></div>
+                    <Input type="email" placeholder="Введите пароль" />
+                </div>
+                <div className="pt-3">
+                    <div><Label htmlFor="terms">Повторите пароль</Label></div>
+                    <Input type="email" placeholder="Повторите пароль" />
+                </div>
+                <div className="pt-3">
+                    <div><Label htmlFor="terms">Выберите роль</Label></div>
+                    <Tabs defaultValue="account" className="w-[400px]">
+                        <TabsList className="grid w-full grid-cols-2">
+                            <TabsTrigger value="Reader">Читатель</TabsTrigger>
+                            <TabsTrigger value="Author">Автор</TabsTrigger>
+                        </TabsList>
+                    </Tabs>
+                </div>
+                <div className="pt-3">
+                    <Button>Создать аккаунт</Button>
+                </div>
 
-            <Button>Создать аккаунт</Button>
+                <div className="pt-3">
+                    <div><Label htmlFor="terms">Уже есть аккаунт?<a href="/login">Войти</a></Label></div>
+                </div>
+            </div>
         </div>
     )
 }
